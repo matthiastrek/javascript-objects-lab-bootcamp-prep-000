3f41d89155a898700d0421a0c10ce4d977f9a265
Matthew Trenk <matt.trenk@gmail.com>
2018-01-31 22:39:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key){
   return delete object.key
+<<<<<<< HEAD
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return object
 }
+=======
+}
+>>>>>>> 4c9ac4037fb257b46bba9d64a6c6d15cb15ef971
